522caed608e72f71584d69a6d868c97735213f2c
Doston1232696 <otkurovdoston69@gmail.com>
2024-10-01 12:47:34 +0500

    Changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+require('dotenv').config();
 
 server.use(middlewares);
 server.use(router);
